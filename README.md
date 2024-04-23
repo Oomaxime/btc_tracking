8032f45a32f5706921c92f7bbeb4ef2e5576213b
Maxime <maxime.bidan33@gmail.com>
2024-04-23 16:31:40 +0200

    API WORKS

---
+++ README.md
@@ -0,0 +1,6 @@
+# BTC TRACKER
+
+## NodeJS training project
+
+API link :
+https://api.coindesk.com/v1/bpi/currentprice.json (use withe Axios)
--- btc.js
+++ btc.js
@@ -1,13 +1,34 @@
-const { createServer } = require("node:http");
-const hostname = "127.0.0.1";
-const port = 3000;
+const { default: axios } = require("axios");
+const process = require("process");
+let args = process.argv;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World</h1>");
-});
+async function getPrice(choice) {
+  const devise = choice.toUpperCase();
+  try {
+    const response = await axios.get(
+      "https://api.coindesk.com/v1/bpi/currentprice.json"
+    );
+    const defaultValue = response.data.bpi["USD"].rate;
+    const value =
+      devise === "EUR" || devise === "USD" || devise === "GBP"
+        ? response.data.bpi[devise].rate
+        : null;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+    //Cette syntax pour 'currencySymbol' vien de chatGPT
+    const currencySymbol = {
+      EUR: "€",
+      USD: "$",
+      GBP: "£",
+    }[devise];
+
+    console.log(
+      value
+        ? `Voici le cours actuel du Bitcoin :\n1 BTC = ${value}${currencySymbol}`
+        : `Erreur ! Mauvais argument !\nDevises disponibles : EUR, USD, GBP`
+    );
+  } catch (error) {
+    console.error("Une erreur s'est produite :", error.message);
+  }
+}
+
+getPrice(args[2]);
